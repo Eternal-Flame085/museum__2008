4446bb541646688acc3adeea8d59e951621b5458
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-10 12:45:29 -0400

    created ticket_lottery_contestants method

--- lib/museum.rb
+++ lib/museum.rb
@@ -37,4 +37,11 @@ class Museum
     end
     exhibit_interests
   end
+
+  def ticket_lottery_contestants(exhibit)
+    patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name) &&
+      patron.spending_money < exhibit.cost
+    end
+  end
 end
