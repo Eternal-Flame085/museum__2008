0418554397332d95e6daf0642c5e04d657b87a07
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-10 11:20:42 -0400

    Created the it has variables test for patron

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,7 +3,7 @@ require "minitest/pride"
 require "./lib/exhibit"
 
 class Test < Minitest::Test
-	def test_it_exist
+	def test_exhibit_exists
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
     assert_instance_of Exhibit, exhibit
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -3,9 +3,17 @@ require "minitest/pride"
 require "./lib/patron"
 
 class Test < Minitest::Test
-	def test_it_exist
+	def test_patron_exists
     patron_1 = Patron.new("Bob", 20)
 
     assert_instance_of Patron, patron_1
 	end
+
+  def test_it_has_variables
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+  end
 end
