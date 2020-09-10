6cd10bec87e9d2e043e0f32115c065d473412ac2
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-10 11:56:41 -0400

    created recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,11 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    exhibits.reduce([]) do |collector, exhibit|
+      collector << exhibit if patron.interests.include?(exhibit.name)
+      collector
+    end
+  end
 end
