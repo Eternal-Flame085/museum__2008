a44bbe94fa08c10b10b4a9d0693cd9d6a3836880
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-10 11:11:53 -0400

    Created test for variables

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -8,4 +8,11 @@ class Test < Minitest::Test
 
     assert_instance_of Exhibit, exhibit
 	end
+
+  def test_it_has_variables
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
 end
